2fe6c28ff9879765ee5c35f4313d539c70f535af
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-05-27 07:45:10 +0300

    Add data picker

--- DatePickerDemo/ViewController.swift
+++ DatePickerDemo/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var datePicker: UIDatePicker = {
+        let picker = UIDatePicker()
+        picker.locale = .current
+        picker.datePickerMode = .dateAndTime
+        picker.preferredDatePickerStyle = .compact
+        picker.tintColor = .systemMint
+        return picker
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+
+        view.addSubview(datePicker)
+        datePicker.center = view.center
+
     }
 
 
